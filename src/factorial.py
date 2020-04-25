01d215b4aaabfdbd9d76b82305048771f5036516
zefetorres <zefetorres@icloud.com>
2020-04-25 17:51:34 -0500

    add factorial

---
+++ src/factorial.py
@@ -0,0 +1,13 @@
+
+def factorial(number):
+    if number == 0:
+        return 1
+    
+    return number * factorial(number - 1)
+
+if __name__ == '__main__':
+    number = int(input('Ingresa un numero: '))
+
+    result = factorial(number)
+
+    print(result)
