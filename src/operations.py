7014691a14450ac65198ec329a07f9b8fbb2a427
zefetorres <zefetorres@icloud.com>
2020-04-25 12:41:01 -0500

    operations

---
+++ src/operations.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+def main():
+    n1 = 20
+    n2 = 10
+    make_sum(n1,n2)
+    make_div(n1,n2)
+
+def make_sum(n1, n2):
+    return print(n1 + n2)
+
+def make_div(n1,n2):
+    return print(n1 / n2)
+
+if __name__ == '__main__':
+    main()
