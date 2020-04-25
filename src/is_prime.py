9a7a2b8f807baf67da8cc7ad6b385676f7f8b8a5
zefetorres <zefetorres@icloud.com>
2020-04-25 16:31:10 -0500

    number is prime?

---
+++ src/is_prime.py
@@ -0,0 +1,26 @@
+
+def is_prime(number):
+    if number < 2:
+        return False
+    elif number == 2:
+        return True
+    elif number > 2 and number % 2 == 0:
+        return False
+    else:
+        for i in range(3, number):
+            if number % i == 0:
+                return False
+    return True
+
+def run():
+    number = int(input('Ingresa un numero:  '))
+
+    result = is_prime(number)
+
+    if result is True:
+        print('Tu número es primo')
+    else:
+        print('Tu número No es primo')
+
+if __name__ == '__main__':
+    run()
